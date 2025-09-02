0c814d77d08879fbd584f77b3186ad78df91a800
VuTuanCuong <114804308+Vucuongtuan@users.noreply.github.com>
2025-09-02 13:28:31 +0700

    Add global collection

---
+++ src/app/(payload)/globals/Menu.ts
@@ -0,0 +1,45 @@
+import { Field, GlobalConfig, Tab } from "payload";
+import { navItem } from "../fields/navItem";
+
+interface MenuFieldProps {
+  label: string;
+  name: string;
+}
+
+const createMenuTab = ({ label, name }: MenuFieldProps): Tab => ({
+  label,
+  name,
+  fields: [
+    {
+      name: "logo",
+      type: "upload",
+      label: "Logo", 
+      relationTo: "media"
+    },
+    {
+      name: "navItems",
+      type: "array",
+      label: "Navigation Items",
+      fields: navItem()
+    }
+  ]
+});
+
+export const Menu: GlobalConfig = {
+  slug: 'menu',
+  fields: [
+    {
+      type: "tabs",
+      tabs: [
+        createMenuTab({
+          label: "Header Menu",
+          name: "header"
+        }),
+        createMenuTab({
+          label: "Footer Menu", 
+          name: "footer"
+        })
+      ]
+    }
+  ]
+};
