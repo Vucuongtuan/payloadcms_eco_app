ac809a508b925b15cd85fcb256aca8fc06a97124
Vucuongtuan <vucuongtuan00@gmail.com>
2025-10-10 16:27:43 +0700

    refactor: remove unused blocks and legacy components

---
+++ src/blocks/(web)/ColumnMedia/Component.tsx
@@ -0,0 +1,56 @@
+import { Media } from "@/components/Media";
+import { cn } from "@/lib/utils";
+import { Media as MediaType } from "@/payload-types";
+import { aspectConfig, layoutCtn } from "@/utilities/cssVariable";
+import Link from "next/link";
+
+export const ColumnMedia = ({ layout, items, columns, aspect }: any) => {
+  const gridColumns = () => {
+    let gridClass = "";
+    switch (columns) {
+      case 1:
+        gridClass = "grid-cols-1";
+        break;
+      case 2:
+        gridClass = "grid-cols-2";
+        break;
+      case 3:
+        gridClass = "grid-cols-2 lg:grid-cols-3";
+        break;
+      case 4:
+        gridClass = "grid-cols-2 lg:grid-cols-3 xl:grid-cols-4";
+        break;
+      default:
+        gridClass = "grid-cols-2";
+    }
+    return `grid ${gridClass}`;
+  };
+  if (!items) return null;
+  return (
+    <div className={cn(layoutCtn(layout || "container"), gridColumns())}>
+      {items.map((item: { media: MediaType; link: any }, idx: number) => (
+        <Link
+          href={item.link?.url || "#"}
+          {...item.link?.newTabProps}
+          key={idx}
+          className={cn(`size-full relative group`, aspectConfig(aspect))}
+        >
+          <Media
+            key={idx}
+            resource={item.media}
+            imgClassName="w-full object-cover"
+            fClassName="relative"
+            fill
+          />
+
+          <div className="absolute  h-full w-full bottom-0 flex flex-col justify-end ">
+            {/* <div className="absolute inset-0 bottom-0 h-1/3 bg-gradient-to-t from-black/50 via-black/50 to-transparent" /> */}
+            <div className="relative z-10 p-4 text-lg lg:text-2xl text-white group-hover:bg-[var(--color-primary)]">
+              {item.link.label}
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
---
+++ src/blocks/(web)/RenderBlocks.tsx
@@ -0,0 +1,57 @@
+import { ContentBlock } from "@/blocks/(web)/Content/Component";
+import { MediaBlock } from "@/blocks/(web)/MediaBlock/Component";
+import { toKebabCase } from "@/utilities/toKebabCase";
+import React, { Fragment } from "react";
+
+import { cn } from "@/lib/utils";
+import { Page } from "@/payload-types";
+import { spacing } from "@/utilities/cssVariable";
+import { CarouselBlock } from "./Carousel/Components";
+import { ColumnMedia } from "./ColumnMedia/Component";
+
+const blockComponents = {
+  content: ContentBlock,
+  mediaBlock: MediaBlock,
+  columnMedia: ColumnMedia,
+  carousel: CarouselBlock,
+};
+export const RenderBlocks: React.FC<{
+  blocks: Page["sections"];
+}> = (props) => {
+  const { blocks } = props;
+  const hasBlocks = blocks && Array.isArray(blocks) && blocks.length > 0;
+
+  if (hasBlocks) {
+    return (
+      <Fragment>
+        {blocks!.map((block, idx) => {
+          const { blockName, blockType } = block;
+
+          if (blockType && blockType in blockComponents) {
+            const Block =
+              blockComponents[blockType as keyof typeof blockComponents];
+            if (Block) {
+              return (
+                <section
+                  className={cn(
+                    idx !== 0 && `${spacing(block.spacing || "none")}`,
+                    idx === 0 && "mt-17"
+                  )}
+                  key={idx}
+                  aria-label={blockName || ""}
+                >
+                  {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                  {/* @ts-ignore - weird type mismatch here */}
+                  <Block id={toKebabCase(blockName!)} {...block} />
+                </section>
+              );
+            }
+          }
+          return null;
+        })}
+      </Fragment>
+    );
+  }
+
+  return null;
+};
