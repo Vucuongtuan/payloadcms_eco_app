a3f6d4fb54ba1ed6c8d3dcf1eab4881848ec4d73
VuTuanCuong <vucuongtuan00@gmail.com>
2025-11-19 16:28:40 +0700

    fix(build):Config turbopack for next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,15 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  turbopack: {
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
+    rules: {
+      "*.svg": {
+        as: "*.js",
+        loaders: ["@svgr/webpack"],
+      },
+    },
+  },
   //Image
   images: {
     remotePatterns: [
